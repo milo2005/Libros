71335fe8fe0d4a20b05e8ebd24f21688a0b1cc99
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-11 16:48:18 -0500

    Atributo de Typeface implementado

--- app/src/main/java/com/example/estacionvl_tc_014/databinding/attrs/Attrs.java
+++ app/src/main/java/com/example/estacionvl_tc_014/databinding/attrs/Attrs.java
@@ -2,8 +2,10 @@ package com.example.estacionvl_tc_014.databinding.attrs;
 
 import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -17,6 +19,13 @@ public class Attrs {
         Uri uri = Uri.parse(url);
         Picasso.with(img.getContext()).load(uri).into(img);
 
+    }
+
+    @BindingAdapter("app:fontTxt")
+    public static void loadFont(TextView txt, String name){
+        Typeface typeface = Typeface.createFromAsset(txt.getContext().getAssets()
+                ,"fonts/"+name+".ttf");
+        txt.setTypeface(typeface);
     }
 
 
